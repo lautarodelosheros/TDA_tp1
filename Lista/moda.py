9b59d0285a22fadde2dfc257a1b04b08aac9bb07
Martín Stefanelli <martin.stefanelli.96@hotmail.com>
2019-04-08 17:22:51 -0300

    moda.py devuelve un numero ya que con encontrar uno solo alcanza

--- Lista/moda.py
+++ Lista/moda.py
@@ -1,19 +1,19 @@
-from maximo import *
+from maximo import maximo
 
 def moda(lista):
-    diccionario_auxiliar = {}
+    diccionario_auxuliar = {}
 
     for numero in lista:
-        diccionario_auxiliar[numero] = diccionario_auxiliar.get(numero, 0) + 1
+        diccionario_auxuliar[numero] = diccionario_auxuliar.get(numero, 0) + 1
 
-    lista_valores = list(diccionario_auxiliar.values())
+    lista_valores = list( diccionario_auxuliar.values() )
     el_mas_grande = maximo(lista_valores)
     repeticion_maxima = 0
-    resultado = []
 
-    for numero in diccionario_auxiliar:
+    for numero in diccionario_auxuliar:
 
-        if diccionario_auxiliar[numero] == el_mas_grande:
-            resultado.append(numero)
+        if diccionario_auxuliar[numero] == el_mas_grande:
+            repeticion_maxima = el_mas_grande
+            break
 
-    return resultado
+    return repeticion_maxima
