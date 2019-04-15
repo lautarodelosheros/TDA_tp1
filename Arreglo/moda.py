16ad7bf72cc4019122eaa3d832573247bbe40828
Martín Stefanelli <martin.stefanelli.96@hotmail.com>
2019-04-15 17:22:52 -0300

    Agrego modificaciones a moda

--- Arreglo/moda.py
+++ Arreglo/moda.py
@@ -8,12 +8,8 @@ def moda(lista):
 
     lista_valores = list( diccionario_auxuliar.values() )
     el_mas_grande = maximo(lista_valores)
-    repeticion_maxima = 0
 
     for numero in diccionario_auxuliar:
 
         if diccionario_auxuliar[numero] == el_mas_grande:
-            repeticion_maxima = el_mas_grande
-            break
-
-    return repeticion_maxima
+            return numero
